3c847e13d6bb53de30e550317d128a915e2afbea
abrusu-collins <snillochemingson@gmail.com>
2022-06-24 20:18:17 +0000

    making headways

--- documentReader/txt/app.js
+++ documentReader/txt/app.js
@@ -7,10 +7,21 @@ let speedgauge = document.getElementById("speed");
 let pitchgauge = document.getElementById("pitch");
 let speedval = document.getElementById("speedval");
 let pitchval = document.getElementById("pitchval");
-let table = document.getElementById("table");
 let play = document.getElementById("play");
 pitchgauge.addEventListener("input", pitchincrease, true);
 speedgauge.addEventListener("input", speedincrease, true);
+
+//file reading
+input.addEventListener('change', function() {
+			
+			var fr=new FileReader();
+			fr.onload=function(){
+				console.log(fr.result)
+			}
+			
+			fr.readAsText(this.files[0]);
+		})
+
 
 function speedincrease(e) {
   speedval.textContent = speedgauge.value;
@@ -39,23 +50,15 @@ if (synth.onvoiceschanged !== undefined) {
 
 function speak(e) {
   e.preventDefault();
-  if (input.value == "") {
-    input.value = "the lazy fox jumped over the brown dog";
+  var fr=new FileReader();
+  
+  if (fr.result == "") {
+    fr.result = "the lazy fox jumped over the brown dog";
   }
 
-  let r = JSON.parse(localStorage.getItem("texts"));
-  if (r === null) {
-    let r = [];
-    r.push(input.value);
-    localStorage.setItem("texts", JSON.stringify(r));
-  } else {
-    let r = JSON.parse(localStorage.getItem("texts"));
+  
 
-    r.push(input.value);
-    localStorage.setItem("texts", JSON.stringify(r));
-  }
-
-  var utterThis = new SpeechSynthesisUtterance(input.value);
+  var utterThis = new SpeechSynthesisUtterance(fr.result);
   var selectedOption = select.selectedOptions[0].getAttribute("data-name");
   for (var i = 0; i < voices.length; i++) {
     if (voices[i].name === selectedOption) {
@@ -69,98 +72,16 @@ function speak(e) {
 
   input.blur();
 
-  if (r.includes(input.value) !== true) {
-    let tbody = document.createElement("tbody");
-    tbody.innerHTML = `<tr class="table-primary">
-    <th scope="row">${input.value}</th>
-    <td id="play">Play</td>
-    <td id="deleted">Delete</td>
-    
-  </tr>`;
-    table.appendChild(tbody);
-  }
+  
 
-  if(table.firstElementChild.id=="no"){
-   
-    let nohistory= document.getElementById("no");
-    nohistory.remove();
-  }
+
 }
 
 button.addEventListener("click", speak, true);
 
-//window load
-window.onload = function (e) {
-  let r = JSON.parse(localStorage.getItem("texts"));
-  let set = new Set(r);
-  let arr = Array.from(set);
-  for (var i = 0; i <= arr.length - 1; i++) {
-    let tbody = document.createElement("tbody");
-    tbody.innerHTML = `<tr class="table-primary">
-      <th scope="row">${arr[i]}</th>
-      <td id="play">Play</td>
-      <td id="deleted">Delete</td>
-      
-    </tr>`;
-    table.appendChild(tbody);
-  }
-  synth.cancel();
 
-  if(table.childElementCount==0){
-    let nohistory=document.createElement("h5");
-    nohistory.innerText=`There is no history`;
-    nohistory.setAttribute("id","no");
-    table.appendChild(nohistory);
-  }
-  
-};
 
-table.addEventListener("click", player, true);
-function player(e) {
-  if (e.target.id == "play") {
-    var utterThis = new SpeechSynthesisUtterance(
-      e.target.parentElement.firstElementChild.textContent
-    );
-    var selectedOption = select.selectedOptions[0].getAttribute("data-name");
-    for (var i = 0; i < voices.length; i++) {
-      if (voices[i].name === selectedOption) {
-        utterThis.voice = voices[i];
-      }
-    }
 
-    utterThis.pitch = pitchgauge.value;
-    utterThis.rate = speedgauge.value;
-    synth.speak(utterThis);
-
-    input.blur();
-  }
-}
-
-table.addEventListener("click", deleter, true);
-function deleter(e) {
-  if (e.target.id == "deleted") {
-    e.target.parentElement.parentElement.remove();
-    let r = JSON.parse(localStorage.getItem("texts"));
-    let j = [];
-    r.filter((rones) => {
-      if (rones !== e.target.parentElement.firstElementChild.textContent) {
-        j.push(rones);
-        synth.cancel();
-      }
-    });
-    localStorage.setItem("texts", JSON.stringify(j));
- console.log(table.textContent);
-    if( JSON.parse(localStorage.getItem("texts")).length==0 && !(table.textContent.includes("There is no history"))){
-   
-      let nohistory=document.createElement("h5");
-      nohistory.innerText=`There is no history`;
-      nohistory.setAttribute("id","no");
-      table.appendChild(nohistory);
-    
-  }}
-
- 
-}
 
 //pause,resume and stop
 
