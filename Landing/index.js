441c2579f65cd424dd8c169c77fe90ec838fd838
abrusu-collins <snillochemingson@gmail.com>
2022-07-10 19:11:26 +0000

    bug fixes

--- Landing/index.js
+++ Landing/index.js
@@ -1,4 +1,6 @@
 let yearp= document.getElementById("year");
 let year= new Date().getFullYear();
 
-yearp.innerHTML=`<span>&copy; ${year} | Abrusu Collins</span>`;
+document.addEventListener("load",()=>{
+    yearp.innerHTML=`<span>&copy; ${year} | Abrusu Collins</span>`;
+})
